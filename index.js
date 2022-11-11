2aeff68132bc664b314b20995f2431ecfdc6bd32
farhanacsebd <farhana.cse.bd@gmail.com>
2022-11-11 23:01:30 +0600

    create form validation function

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
 // link up html with javascript 
 const form = document.getElementById('form');
+const textInput = document.getElementById('textInput');
+const dateInput = document.getElementById('dateInput');
+const textareaInput = document.getElementById('textareaInput');
+const msg = document.querySelector('.msg');
 
 
+
+// connect button with form using addend
 form.addEventListener('submit',function(e){
     e.preventDefault()
-    console.log('hello');
+    formValidation()
 })
+
+
+// form Validation function
+function  formValidation(){
+    if(textInput.value === ''){
+        console.log('fail');
+        msg.innerHTML = `Task cannot be blank`
+    }
+    else{
+        console.log('success');
+        msg.innerHTML ="";
+    }
+}
+
+
+let data = {};
+
+function acceptData(){
+    
+}
